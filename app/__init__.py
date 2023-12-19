3db0a00459206773f00e9e738fc079a256895f57
jen5000 <jenny.pretzsch@robotron.de>
2023-12-19 01:27:57 +0100

    changed the app index file and uncommented the auth section

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,209 @@
 import os
+import json
+from PIL import Image
+import sys
+import requests
+import urllib.parse
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, render_template, request
+from flask_login import LoginManager, login_required
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
+# TODO: does this work like this or do i need to call it auth
+from app.auth import User
+from app.util.custom_vision import custom_vision_predict, custom_vision_classify_moncherie
+from app.util.image_helpers import draw_bb_on_img
+from app.util.gradio_call import call_gradio_api
+from app.util.transcription import whisper_transcribe
+from app.util.image_helpers import save_img
 
 
+from .auth import auth as auth_blueprint
+
+def create_app():
+    app = Flask(__name__)
+    # TODO: add to power point
+    app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
+
+    app.register_blueprint(auth_blueprint)
+
+    # TODO: add to power point
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    # TODO: what does this do?
+    @login_manager.user_loader
+    def load_user(user_id):
+        user = User()
+        user.id = "Adam"
+        return user
+    
+    # TODO: add the login manager
+    return app
+
+app = create_app()
+
+# TODO: make blueprints and somehow do cluster the functions <vision_version> or something
 @app.route('/')
+@login_required
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    # TODO: there might be a nicer way to do this....
+    nav = [
+       {'name': 'Schweißnähte', 'url': '/wheelding', 'img': '/static/images/sample_wheelding.jpg'},
+       {'name': 'MonCherie', 'url': '/moncherie', 'img' : "/static/images/moncherie2.png"},
+       {'name': 'Cups', 'url': '/cups',  'img' : "/static/images/cup.jpg"},
+       {"name": "Huggingface Whisper Inference API", 'url': '/audio_upload', 'img' : '/static/images/sample_wheelding.jpg'},
+       {'name': 'SpeechProtocol', 'url': '/gradio', 'img': '/static/images/sample_wheelding.jpg'}
+       ]
+    return render_template('index.html', nav=nav)
 
 
+# TODO: clean up the routes and the names to make the consistent
+@app.route('/wheelding')
+@login_required
+def upload_wheelding():
+    return render_template("upload.html", processing_action="/process_wheelding")
+
+
+@app.route('/process_wheelding', methods=['POST'])
+@login_required
+def process_wheelding():
+
+    selected_image, img_path = save_img(request)
+
+    mode = "wheelding"
+    predictions = custom_vision_predict(img_path, mode)
+
+    img_with_bb =  draw_bb_on_img(selected_image, predictions, mode, threshold=0.5, threshold_2=0.1)
+
+    return render_template("wheelding.html", img_path=img_with_bb)
+
+
+@app.route('/moncherie')
+@login_required
+def upload_moncherie():
+    return render_template("upload.html", processing_action="/process_moncherie")
+
+
+@app.route("/process_moncherie", methods=['POST'])
+@login_required
+def process_moncherie():
+
+    selected_image, img_path = save_img(request)
+
+    mode = "moncherie"
+    predictions = custom_vision_predict(img_path, mode)
+    # TODO: this could be like a slider or some other kind of input
+    threshold = 0.5
+    threshold_2 = 0.1
+    img_with_bb =  draw_bb_on_img(selected_image, predictions, mode, threshold, threshold_2)
+
+    biggest_probablity, winner_label, classifier_result = custom_vision_classify_moncherie(img_path)
+    rendered_result = "Most certainly (" + str(biggest_probablity * 100)[:5] + "%) the box is " + winner_label 
+
+    return render_template("moncherie.html", info=rendered_result, classification_info = str(classifier_result), img_obj=img_with_bb)
+
+
+@app.route("/cups")
+@login_required
+def cups_page():
+    return render_template("upload.html", processing_action="/process_cups")
+
+
+@app.route("/process_cups", methods=['POST'])
+@login_required
+def process_cups():
+    selected_image, img_path = save_img(request)
+    
+    mode = "cups"
+    predictions = custom_vision_predict(img_path, mode)
+    # TODO: this could be like a slider or some other kind of input
+    threshold = 0.5
+    threshold_2 = 0.1
+    img_with_bb =  draw_bb_on_img(selected_image, predictions, mode, threshold, threshold_2)
+
+    return render_template("cups.html", img_obj=img_with_bb)
+
+
+@app.route("/audio_upload")
+@login_required
+def upload_audio():
+    return render_template("upload.html", processing_action="/hf_whisper")
+
+
+@app.route("/hf_whisper")
+@login_required
+def call_inference_api_whisper():
+    # TODO: if this works, rename to data_input or something..
+    whisper_transcribe(request.files['image'])
+    return render_template("speech_protocol.html")
+
+
+@app.route("/get_url_for_caption")
+@login_required
+def get_url_for_caption():
+    return render_template("image_url.html")
+
+
+@app.route("/captions")
+@login_required
+def call_computer_vision_api():
+    # get the url input
+    # TODO: check weather the input is valid...
+    # TODO...
+    img_url = request.form.get('img_url')
+
+    if 'COMPUTER_VISION_KEY' in os.environ:
+        subscription_key = os.environ['COMPUTER_VISION_KEY']
+    else:
+        print("\nSet the COMPUTER_VISION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+        sys.exit()
+
+    if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+        endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+
+    analyze_url = endpoint + "computervision/imageanalysis:analyze?api-version=2023-10-01&%s"
+
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+    params = urllib.parse.urlencode({
+        'features': 'denseCaptions', # TODO: syntax for multiple features 
+        'model-version': 'latest',
+        'language': 'en',
+    })
+    data = {'url': img_url}
+
+    response = requests.post(analyze_url, headers=headers,
+                            params=params, json=data)
+    response.raise_for_status()
+    analysis = response.json()
+    return json.dumps(response.json())
+
+
+@app.route('/gradio')
+@login_required
+def show_gradio_app():
+    return render_template("speech_protocol.html")
+
+
+# TODO: this is unused, delete or something...
+@app.route('/speech_protocol')
+@login_required
+def generate_protocol():
+    # TODO: use this file?? what for?
+    if request.method == "POST":
+        file = request.files['file']
+        if file:
+            audio_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            return render_template('speech_protocol.html', result=call_gradio_api(audio_path))
+    # TODO: include the gradio call
+    # this page has the gradio space embedded
+    return render_template('speech_protocol.html', result="no result, wrong path")
+
+
+
+
+# TODO: brauche ich das?
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
+ 
--- app/auth.py
+++ app/auth.py
@@ -1,55 +1,55 @@
-# from flask import Blueprint, render_template, redirect, url_for, request
-# from flask_login import UserMixin, login_user, logout_user
-# import os
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask_login import UserMixin, login_user, logout_user
+import os
 
-# class User(UserMixin):
-#     # TODO: what is this for?
-#     id = "an id"
-
-
-# auth = Blueprint('auth', __name__)
+class User(UserMixin):
+    # TODO: what is this for?
+    id = "an id"
 
 
-# @auth.route('/login')
-# def login():
-#     return render_template('login.html')
+auth = Blueprint('auth', __name__)
 
 
-# @auth.route('/login', methods=['POST'])
-# def login_post():
-#     """checks wheather the provided credentials are the same as
-#     the ones stored in the enviroment variables
-#     if so a login user is created and the logged in user can view
-#     the index page
+@auth.route('/login')
+def login():
+    return render_template('login.html')
 
-#     Returns:
-#         BaseResponse: redirection to the index page on success
-#         BaseResponse: redirection to the login page on failure
-#     """
-#     username = request.form.get('username')
-#     password = request.form.get('password')
 
-#     env_var_username = os.environ['username']
-#     env_var_password = os.environ['password']
+@auth.route('/login', methods=['POST'])
+def login_post():
+    """checks wheather the provided credentials are the same as
+    the ones stored in the enviroment variables
+    if so a login user is created and the logged in user can view
+    the index page
 
-#     if (username == env_var_username):
-#         if (password == env_var_password):
-#             user = User()
-#             user.id = "Schnuser"
-#             login_user(user)
-#             return redirect(url_for('index'))
+    Returns:
+        BaseResponse: redirection to the index page on success
+        BaseResponse: redirection to the login page on failure
+    """
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    env_var_username = os.environ['username']
+    env_var_password = os.environ['password']
+
+    if (username == env_var_username):
+        if (password == env_var_password):
+            user = User()
+            user.id = "Schnuser"
+            login_user(user)
+            return redirect(url_for('index'))
         
-#     return redirect(url_for('auth.login'))
+    return redirect(url_for('auth.login'))
 
-# @auth.route('/logout')
-# def logout():
-#     """when the logout route is called, the user gets redirected to the
-#     login route
+@auth.route('/logout')
+def logout():
+    """when the logout route is called, the user gets redirected to the
+    login route
 
-#     Returns:
-#         BaseResponse: "it’s a regular WSGI application", sends the user to the login page
-#     """
-#     logout_user()
-#     return redirect(url_for('auth.login'))
+    Returns:
+        BaseResponse: "it’s a regular WSGI application", sends the user to the login page
+    """
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
