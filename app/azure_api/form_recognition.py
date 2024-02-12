4d4bd6219e9544be491cdc7f071871edce7c83ba
jen5000 <jenny.pretzsch@robotron.de>
2024-02-12 13:49:22 +0100

    added keys to azure and some print stmts for debugging

--- app/azure_api/form_recognition.py
+++ app/azure_api/form_recognition.py
@@ -8,8 +8,11 @@ def call_API_Receipt(img_path):
     key = os.environ["FORMULAR_KEY"]
     endpoint = os.environ["FORMULAR_ENDPOINT"]
 
+    print("Keys exists")
+
     document_analysis_client = DocumentAnalysisClient(endpoint=endpoint, credential=AzureKeyCredential(key))
     with open(img_path, 'rb') as img:
+      print("image could be opened")
       poller = document_analysis_client.begin_analyze_document("prebuilt-receipt", img)
 
     return poller.result()
